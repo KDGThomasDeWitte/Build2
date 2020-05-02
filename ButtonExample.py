ae6e82ab2912e12f4125deebb33ee827f18ddcc0
KDGThomasDeWitte <[thomas.dewitte@kdg.be]>
2020-05-02 15:36:59 +0200

    created

---
+++ ButtonExample.py
@@ -0,0 +1,6 @@
+# Library Import
+from gpiozero import DigitalOutputDevice  # DIGITALOUTPUTDEVICE is for RELAIS
+from gpiozero import Button
+from gpiozero.pins.pigpio import PiGPIOFactory
+import time
+
