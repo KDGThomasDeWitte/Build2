9127af080f4af6e3275dadd4f8f5fe0d59fa021f
KDGThomasDeWitte <[thomas.dewitte@kdg.be]>
2020-05-03 14:08:30 +0200

    Delete ButtonExample.py

--- ButtonExample.py
+++
@@ -1,6 +0,0 @@
-# Library Import
-from gpiozero import DigitalOutputDevice  # DIGITALOUTPUTDEVICE is for RELAIS
-from gpiozero import Button
-from gpiozero.pins.pigpio import PiGPIOFactory
-import time
-
